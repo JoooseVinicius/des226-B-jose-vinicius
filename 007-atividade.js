c47cfe9241b5a42d762c6f873bf67f2b72a8b5ce
Zeh <notoriosgamermito@gmail.com>
2026-09-01 20:38:52 -0300

    não sei o que tentei fazer

---
+++ 007-atividade.js
@@ -0,0 +1,20 @@
+let entrada = require("prompt-sync")();
+
+let produto = 'Mesa'
+let nome = 'Mesa-Gamer'
+let preço = '300$'
+let disponibilidade = true;
+let categorias = ['Eletrônicos', 'Periféricos', 'Gamer']
+
+console.log();
+
+console.log(produto);
+console.log(nome, preço, disponibilidade);
+console.log(categorias);
+console.log('Mesa'+ produto + typeof 'produto' );
+
+
+
+
+
+
