7c33f002adc06b58c505e65ce58c3389a5063882
Zeh <notoriosgamermito@gmail.com>
2026-09-04 20:39:21 -0300

    ATIVADE FULL IA, DANESSE

--- 007-atividade.js
+++ 007-atividade.js
@@ -1,20 +1,15 @@
-let entrada = require("prompt-sync")();
+let produto = {
+    nome: "Produto",
+    preco: 500,
+    disponivel: true,
+    tags: ['eletrônicos', 'hardware', 'computador']
+};
 
-let produto = 'Mesa'
-let nome = 'Mesa-Gamer'
-let preço = '300$'
-let disponibilidade = true;
-let categorias = ['Eletrônicos', 'Periféricos', 'Gamer']
+console.log('ficha de produto:');
+console.log (produto);
 
-console.log();
+console.log('Tipo da variável do produto:');
+console.log(typeof produto);
 
-console.log(produto);
-console.log(nome, preço, disponibilidade);
-console.log(categorias);
-console.log('Mesa'+ produto + typeof 'produto' );
-
-
-
-
-
-
+console.log('Tipo do Array');
+console.log(typeof produto.tags);
